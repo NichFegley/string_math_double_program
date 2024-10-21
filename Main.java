2b857133e53fe306726e7670e3c611ab8084e654
NichFegley <26nfegley@students.bhasd.org>
2024-10-21 08:33:28 -0400

    Finalized Project

--- Main.java
+++ Main.java
@@ -1,8 +1,20 @@
 public class Main
 {
-    public static void Main (String[]args)
+    public static void main (String[]args)
     {
-        //your code goes here
+        //hard code output
+        String inputStr = "The cost of the T-shirt is $10.95.";
+        String price = inputStr.substring(28,33);
+        double priceAsDouble = Double.parseDouble(price);
+        int discount = (int)(Math.random() * 30) + 1;
+        double finalCost = priceAsDouble * (1-discount/100.0);
+        String finalCostAsString= String.format("%.2f", finalCost);
+        System.out.println(priceAsDouble);
+        System.out.println(inputStr);
+        System.out.println("the discount of the day is " + discount + "% off the original cost");
+        System.out.println("The final cost of the T-shirt is $"+finalCostAsString+ ".");
+
+
 
     }
 
